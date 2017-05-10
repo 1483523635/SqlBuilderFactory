1df7ebc8454a129d39412f1a1ce347f0734c912f
曲廉卿 <1483523635@qq.com>
2017-05-10 14:08:46 +0800

    add a fileForder and create two class

---
+++ Reflect/Reflect/DateBaseFactory/Implement/SqlServerDataBase.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Reflect.DateBaseFactory.BaseClass;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Net.Http.Headers;
+
+namespace Reflect.DateBaseFactory.Implement
+{
+    public class SqlServerDatabase : BasicsDatabase
+    {
+        private SqlBuilderFactory SqlBuilder;
+
+        public SqlServerDatabase() : base()
+        {
+            base.Connection = new SqlConnection(DatabaseConncetionString());
+            Connection.Open();
+            base.Command = new SqlCommand();
+            Command.Connection = base.Connection;
+            SqlBuilder=new SqlBuilderFactory();
+        }
+
+        public override List<T> GetAllInfo<T>()
+        {
+            throw new NotImplementedException();
+        }
+        public override bool Add(object data)
+        {       
+            Command.CommandText = SqlBuilder.CreateSqlString(data);
+            return Command.ExecuteNonQuery() > 0;
+        }
+        public override bool Remove(object data)
+        {
+            Command.CommandText = SqlBuilder.DeleteSqlString(data);
+            return Command.ExecuteNonQuery() > 0;
+        }
+
+        public override bool Update(object data)
+        {
+            Command.CommandText = SqlBuilder.UpdateSqlString(data);
+            return Command.ExecuteNonQuery() > 0;
+        }
+
+        protected override string DatabaseConncetionString()
+        {
+            return base.Configuration.GetConnectionString("UserInfoContext");
+        }
+
+
+    }
+}
